ca1116d462d04da7e69706160d29e09ca06fde6e
archangelsim <angel.stan.simeonov@gmail.com>
2022-09-24 11:38:19 +0300

    Town, Population, Area

---
+++ town-populartion-area.js
@@ -0,0 +1,9 @@
+function townPopulationArea(town, population, area) {
+    //  "Town {town name} has population of {population} and area {area} square km."
+
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`)
+}
+
+townPopulationArea('Sofia',
+1286383,
+492)
